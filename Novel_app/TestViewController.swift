3d22ac3735e555eb6fd5862517981b89d51d95d2
A3426L <mikan7738@gmail.com>
2024-06-01 18:03:10 +0900

    Add:　TabBarからのモーダル遷移

---
+++ Novel_app/TestViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TestViewController.swift
+//  Novel_app
+//
+//  Created by aru on 2024/06/01.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
